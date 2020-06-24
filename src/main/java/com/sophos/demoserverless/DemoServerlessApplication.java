8d23144a2907b384b6ef9f8caccab581ba6d4912
Ricardo Ramírez <rickyrb@gmail.com>
2020-06-24 16:36:53 -0500

    * Versión operativa en Lambda

--- src/main/java/com/sophos/demoserverless/DemoServerlessApplication.java
+++ src/main/java/com/sophos/demoserverless/DemoServerlessApplication.java
@@ -1,12 +1,9 @@
 package com.sophos.demoserverless;
 
-import com.sophos.demoserverless.controller.EmpleadoController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({EmpleadoController.class})
 public class DemoServerlessApplication {
 
 	public static void main(String[] args) {
