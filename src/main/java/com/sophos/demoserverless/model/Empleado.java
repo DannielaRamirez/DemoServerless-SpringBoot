2eef7069f05892171ad25b831c17040f5ce7c448
Ricardo José Ramírez Blauvelt <ricardo.ramirez@sophossolutions.com>
2020-07-07 14:38:59 -0500

    * Se continúa con la implementación de Lombok y se utiliza en la Entidad * Se añade el archivo de configuración de Lombok para que la prueba de cobertura con JaCoCo sea coherente

--- src/main/java/com/sophos/demoserverless/model/Empleado.java
+++ src/main/java/com/sophos/demoserverless/model/Empleado.java
@@ -1,7 +1,9 @@
 package com.sophos.demoserverless.model;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.*;
+import lombok.Data;
 
+@Data
 @DynamoDBTable(tableName = "DemoServerless")
 public class Empleado {
 
@@ -25,61 +27,5 @@ public class Empleado {
 
 	@DynamoDBAttribute
 	private String busqueda;
-
-	public String getHk() {
-		return hk;
-	}
-
-	public void setHk(String hk) {
-		this.hk = hk;
-	}
-
-	public String getSk() {
-		return sk;
-	}
-
-	public void setSk(String sk) {
-		this.sk = sk;
-	}
-
-	public String getCedula() {
-		return cedula;
-	}
-
-	public void setCedula(String cedula) {
-		this.cedula = cedula;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public Integer getEdad() {
-		return edad;
-	}
-
-	public void setEdad(Integer edad) {
-		this.edad = edad;
-	}
-
-	public String getCiudad() {
-		return ciudad;
-	}
-
-	public void setCiudad(String ciudad) {
-		this.ciudad = ciudad;
-	}
-
-	public String getBusqueda() {
-		return busqueda;
-	}
-
-	public void setBusqueda(String busqueda) {
-		this.busqueda = busqueda;
-	}
 
 }
